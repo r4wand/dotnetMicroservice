9f2656fe4eae5b32a530fa748851aaa766187b78
Rawand <26229485+r4wand@users.noreply.github.com>
2021-12-02 10:50:12 +0300

    Added book model

---
+++ Models/Book.cs
@@ -0,0 +1,19 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace microservice.Models;
+
+
+public class Book
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("Name")]
+    public string BookName { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public string Author { get; set; } = null!;
+}
