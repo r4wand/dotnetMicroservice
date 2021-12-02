0b6788e0708e05471d138d4343d4622dfa32dc4e
Rawand <26229485+r4wand@users.noreply.github.com>
2021-12-02 11:52:52 +0300

    Added BookService singleton

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using microservice.Models;
+using microservice.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.Configure<BookStoreDatabaseSettings>(
     builder.Configuration.GetSection("BookStoreDatabase"));
+
+builder.Services.AddSingleton<BooksService>();
 
 var app = builder.Build();
 
